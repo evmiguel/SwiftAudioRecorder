fc70c31c2a33db817b053ec4e28fdd1193bb190d
Erika V. Miguel <emiguel@ford-243-08.local>
2014-10-23 11:39:23 -0400

    Initial Commit

---
+++ SoundRecorder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SoundRecorder
+//
+//  Created by Erika V. Miguel on 10/23/14.
+//  Copyright (c) 2014 Erika V. Miguel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
