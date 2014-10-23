ea49a69bedb4d2467fc32f40a7ed2d5e68168710
Erika V. Miguel <emiguel@ford-243-08.local>
2014-10-23 16:49:07 -0400

    Updated recorders

--- SoundRecorder/ViewController.swift
+++ SoundRecorder/ViewController.swift
@@ -17,15 +17,9 @@ class ViewController: UIViewController, AVAudioRecorderDelegate {
     @IBOutlet weak var stopButton: UIButton!
     @IBOutlet weak var playButton: UIButton!
     
-    var recordSettings = [
-        AVFormatIDKey: kAudioFormatAppleLossless,
-        AVEncoderAudioQualityKey : AVAudioQuality.Max.toRaw(),
-        AVEncoderBitRateKey : 320000,
-        AVNumberOfChannelsKey: 2,
-        AVSampleRateKey : 44100.0
-    ]
-    
     var recorder = AVAudioRecorder()
+    var player = AVAudioPlayer()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,11 +28,18 @@ class ViewController: UIViewController, AVAudioRecorderDelegate {
         stopButton.enabled = false
         playButton.enabled = false
         
-        var error: NSError?
+
         let paths = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
         var documentsURL = paths[0] as NSURL
         let soundFileURL = documentsURL.URLByAppendingPathComponent("sound.mp3")
-        
+        var recordSettings = [
+            AVFormatIDKey: kAudioFormatAppleLossless,
+            AVEncoderAudioQualityKey : AVAudioQuality.Max.toRaw(),
+            AVEncoderBitRateKey : 320000,
+            AVNumberOfChannelsKey: 2,
+            AVSampleRateKey : 44100.0
+        ]
+        var error: NSError?
         recorder = AVAudioRecorder(URL: soundFileURL, settings: recordSettings, error: &error)
         if let e = error {
             println(e.localizedDescription)
@@ -49,6 +50,29 @@ class ViewController: UIViewController, AVAudioRecorderDelegate {
         }
         
         
+        
+    }
+    
+    @IBAction func recordTapped(sender: AnyObject) {
+        if player.playing {
+            player.stop()
+        }
+        
+        if !recorder.recording {
+            var session = AVAudioSession.sharedInstance()
+            session.setActive(true, withOptions: nil, error: nil)
+            recorder.record()
+            recordButton.setTitle("Pause", forState: UIControlState.Normal)
+        } else {
+            recorder.pause()
+            recordButton.setTitle("Record", forState: UIControlState.Normal)
+        }
+    }
+    
+    @IBAction func stopTapped(sender: AnyObject) {
+        recorder.stop()
+        var audioSession = AVAudioSession.sharedInstance()
+        audioSession.setActive(false, withOptions: nil, error: nil)
         
     }
     
@@ -64,16 +88,15 @@ class ViewController: UIViewController, AVAudioRecorderDelegate {
             recordButton.setTitle("Record", forState:.Normal)
             
             // ios8 and later
-            var alert = UIAlertController(title: "Recorder",
+/*            var alert = UIAlertController(title: "Recorder",
                 message: "Finished Recording",
                 preferredStyle: .Alert)
             alert.addAction(UIAlertAction(title: "Keep", style: .Default, handler: {action in
                 println("keep was tapped")
             }))
-            alert.addAction(UIAlertAction(title: "Delete", style: .Default, handler: {action in
-                self.recorder.deleteRecording()
-            }))
-            self.presentViewController(alert, animated:true, completion:nil)
+            alert.addAction(UIAlertAction(title: "Delete", style: .Default, handler: {action in self.recorder.deleteRecording()})) */
+            
+          //  self.presentViewController(alert, animated:true, completion:nil)
     }
     
     func audioRecorderEncodeErrorDidOccur(recorder: AVAudioRecorder!,
