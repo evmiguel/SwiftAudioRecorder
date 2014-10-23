df0ae60860a77f236bea9dcfeafd4604bffdc10c
Erika V. Miguel <emiguel@ford-243-08.local>
2014-10-23 11:42:25 -0400

    first commit

---
+++ SoundRecorder/SoundRecorder.swift
@@ -0,0 +1,9 @@
+//
+//  SoundRecorder.swift
+//  SoundRecorder
+//
+//  Created by Erika V. Miguel on 10/23/14.
+//  Copyright (c) 2014 Erika V. Miguel. All rights reserved.
+//
+
+import Foundation
