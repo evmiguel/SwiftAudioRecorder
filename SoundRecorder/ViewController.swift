67e87c076969967197401d2a74618b68591da7e1
Erika V. Miguel <emiguel@ford-243-08.local>
2014-10-23 16:03:50 -0400

    Second commit

--- SoundRecorder/ViewController.swift
+++ SoundRecorder/ViewController.swift
@@ -6,20 +6,81 @@
 //  Copyright (c) 2014 Erika V. Miguel. All rights reserved.
 //
 
+//TODO: Rename buttons, need to reconnect to viewcontroller ad disconnect
+
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, AVAudioRecorderDelegate {
+    
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    @IBOutlet weak var playButton: UIButton!
+    
+    var recordSettings = [
+        AVFormatIDKey: kAudioFormatAppleLossless,
+        AVEncoderAudioQualityKey : AVAudioQuality.Max.toRaw(),
+        AVEncoderBitRateKey : 320000,
+        AVNumberOfChannelsKey: 2,
+        AVSampleRateKey : 44100.0
+    ]
+    
+    var recorder = AVAudioRecorder()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        stopButton.enabled = false
+        playButton.enabled = false
+        
+        var error: NSError?
+        let paths = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
+        var documentsURL = paths[0] as NSURL
+        let soundFileURL = documentsURL.URLByAppendingPathComponent("sound.mp3")
+        
+        recorder = AVAudioRecorder(URL: soundFileURL, settings: recordSettings, error: &error)
+        if let e = error {
+            println(e.localizedDescription)
+        } else {
+            recorder.delegate = self
+            recorder.meteringEnabled = true
+            recorder.prepareToRecord() // creates/overwrites the file at soundFileURL
+        }
+        
+        
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    func audioRecorderDidFinishRecording(recorder: AVAudioRecorder!,
+        successfully flag: Bool) {
+            println("finished recording \(flag)")
+            stopButton.enabled = false
+            playButton.enabled = true
+            recordButton.setTitle("Record", forState:.Normal)
+            
+            // ios8 and later
+            var alert = UIAlertController(title: "Recorder",
+                message: "Finished Recording",
+                preferredStyle: .Alert)
+            alert.addAction(UIAlertAction(title: "Keep", style: .Default, handler: {action in
+                println("keep was tapped")
+            }))
+            alert.addAction(UIAlertAction(title: "Delete", style: .Default, handler: {action in
+                self.recorder.deleteRecording()
+            }))
+            self.presentViewController(alert, animated:true, completion:nil)
+    }
+    
+    func audioRecorderEncodeErrorDidOccur(recorder: AVAudioRecorder!,
+        error: NSError!) {
+            println("\(error.localizedDescription)")
+    }
+    
 }
+
 
